6b8ad15c55fdfa9902086809a9fc6f692b7fc5d2
ahahau <ahahau1@naver.com>
2024-08-27 01:37:46 +0900

    wedwed

---
+++ Assets/Scripts/AppTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppTest : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("µûÀÕ");
+            Application.OpenURL("https://www.youtube.com/");
+        }
+    }
+}
